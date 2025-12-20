53d2ee685a216329795fb6f2356f8068d9539e1c
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-12-21 00:39:51 +0530

    Add files via upload

---
+++ encapsulation.java
@@ -0,0 +1,17 @@
+public class encapsulation {
+
+    private String brand_name;
+    private String owner_name;
+    private int model_year;
+    private double Demanded_price;
+    public void setBrandName(String brand_name) {
+        this.brand_name = brand_name;
+    public String getBrandName() {
+        return brand_name;
+    public void setOwnerName(String owner_name) {
+        this.owner_name = owner_name;
+    public String getOwnerName(){
+        return owner_name;}
+    
+    }
+}
