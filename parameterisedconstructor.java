addef7bfce73e9c5ab59f0031a437246b8faaaf1
Suryansh tripathii <suryanshtripathi778@gmail.com>
2025-12-25 10:23:59 +0530

    Add files via upload

---
+++ parameterisedconstructor.java
@@ -0,0 +1,8 @@
+class car{
+    String model;
+    int price;
+    public void display(){System.out.println("lets seee whoo callled first constructor or this display function");}
+car(String a ;int b;){String model=a;
+    int price=b;
+}
+}
